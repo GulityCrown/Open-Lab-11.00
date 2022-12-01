446e325fe66ad1b434c8b1fed96ed4ae9c7b356f
GulityCrown <cupalukas2@gmail.com>
2022-12-01 20:07:10 +0100

    Add project files.

---
+++ Open-Lab-11.00/Book.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_11._00
+{
+    internal class Book
+    {
+        /// <summary>
+        /// list of categories
+        /// </summary>
+        public static List<string> categoryList = new List<string>() { "detské", "romantické", "náučné", "sci-fi", "dobrodružné" };
+
+        private string title;
+        private string category;
+        private string author;
+        private int relaseDate;
+        private int pages;
+        /// <summary>
+        /// set/get property for title
+        /// </summary>
+        public string Title { get { return title; } set { title = value; } }
+        /// <summary>
+        /// set/get property for category
+        /// </summary>
+        public string Category
+        {
+            set { category = value; }
+        }
+        /// <summary>
+        /// set/get property for author
+        /// </summary>
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        /// <summary>
+        /// set/get property for relaseDate
+        /// </summary>
+        public int RelaseDate
+        {
+            get => relaseDate;
+            set
+            {
+                if ((value >= 2021) || (value <= 1450))
+                {
+                    relaseDate = -1;
+                }
+                else
+                {
+                    relaseDate = value;
+                }
+            }
+
+        }
+        /// <summary>
+        /// set/get property for pages
+        /// </summary>
+        public int Pages
+        {
+            get { return pages; }
+            set
+            {
+                pages = value;
+            }
+        }
+        /// <summary>
+        /// non paramethrized constructor that sets every field to -1
+        /// </summary>
+        public Book()
+        {
+            title = "-1";
+            category = "-1";
+            author = "-1";
+            relaseDate = -1;
+            pages = -1;
+        }
+        /// <summary>
+        /// parametherized constructor that sets title and pages(constructor overloading)
+        /// </summary>
+        public Book(string xTitle, int xPages)
+        {
+            this.title = xTitle;
+            this.category = "-1";
+            this.author = "-1";
+            this.relaseDate = -1;
+            this.pages = xPages;
+        }
+        /// <summary>
+        /// parametherized constructor that sets every field(constructor overloading)
+        /// </summary>
+        public Book(string xTitle, int xPages, string xCategory, string xAuthor, int xRelaseDate)
+        {
+            title = xTitle;
+            category = xCategory;
+            author = xAuthor;
+            relaseDate = xRelaseDate;
+            pages = xPages;
+        }
+
+
+        /// <summary>
+        /// method that reads every field
+        /// </summary>
+        public void Vypis()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Category: " + category);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Relase date: " + relaseDate);
+            Console.WriteLine("number of pages: " + pages);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Open-Lab-11.00/Program.cs
@@ -0,0 +1,33 @@
+﻿using Open_Lab_11._00;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_11._00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR2 = new Book();
+
+            /*LOTR.Title = "Lord Of The Rings";
+            LOTR.Author = "J.J.R.Tolkien";
+            LOTR.Category = "epic high-fantasy novel";
+            LOTR.Pages = 1172;
+            LOTR.RelaseDate = "29th July 1954";*/
+            LOTR2.Vypis();
+            Book LOTR3 = new Book("Lord of the Rings 3", 512);
+            LOTR3.Vypis();
+            Book HOBBIT = new Book("Hobbit", 310, "juvenile fantasy", "J.R.R.Tolkien", 1937);
+
+            HOBBIT.Category = Book.categoryList[4];
+            HOBBIT.Vypis();
+
+            Console.WriteLine("LOTR3 title is: " + HOBBIT.Pages);
+        }
+    }
+}
+
